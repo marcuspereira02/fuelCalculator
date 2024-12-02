6e2c54c35c2007480e5798c5adccb12758d86b55
Marcus Vinícius de Sá Pereira <makitasjc@outlook.com>
2024-12-02 15:18:40 -0300

    Layout da tela inicial e inicio do layout da segunda tela

---
+++ app/src/main/java/com/marcuspereira/fuelcalculator/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.marcuspereira.fuelcalculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+            val btnIniciar = findViewById<Button>(R.id.btn_iniciar)
+
+            btnIniciar.setOnClickListener{
+               val intent = Intent(this,PriceActivity::class.java)
+               intent.putExtra(KEY_PRICE_ACTIVITY,"")
+                startActivity(intent)
+            }
+        }
+    }
---
+++ app/src/main/java/com/marcuspereira/fuelcalculator/PriceActivity.kt
@@ -0,0 +1,17 @@
+package com.marcuspereira.fuelcalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+const val KEY_PRICE_ACTIVITY =  "PriceActivity.Key"
+
+class PriceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_price)
+    }
+}
